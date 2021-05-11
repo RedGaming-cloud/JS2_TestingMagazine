dd89359213af818d10233663589c8c204e5c3083
Юрий <frunzeun@gmail.com>
2021-05-11 23:28:40 +0300

    lesson6 HW

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+        showCart: false,
+        cartUrl: '/getBasket.json',
+        catalogUrl: '/catalogData.json',
+        products: [],
+        cartItems: [],
+        filtered: [],
+        imgCatalog: 'https://placehold.it/200x150',
+        imgCart: 'https://placehold.it/50x100'
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        filter() {
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    mounted() {
+
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+    }
+})
+
